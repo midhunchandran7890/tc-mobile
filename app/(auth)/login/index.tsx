7a789a817bbbe4ed0b17666f088b2119d2908563
midhunchandran7890 <midhunchandran7890@gmail.com>
2025-08-20 21:21:16 +0530

    Login screen created

---
+++ app/(auth)/login/index.tsx
@@ -0,0 +1,13 @@
+import { Text, View } from "react-native";
+
+import { styles } from "./styles";
+
+const Login = () => {
+  return (
+    <View style={styles.container}>
+      <Text>Login Screen</Text>
+    </View>
+  );
+};
+
+export default Login;
