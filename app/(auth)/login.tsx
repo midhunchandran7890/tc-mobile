8e24b571bcdf09b379149811968bfcbc88df41cf
midhunchandran7890 <midhunchandran7890@gmail.com>
2025-08-28 21:16:52 +0530

    Login screen basic ui

--- app/(auth)/login.tsx
+++ app/(auth)/login.tsx
@@ -1,21 +1,38 @@
-import { Text, View } from "react-native";
+import { Text, TextInput, TouchableOpacity, View } from "react-native";
 
+import { Image } from "expo-image";
 import { styles } from "../../styles/login";
 
 const Login = () => {
   return (
     <View style={styles.container}>
-      <View>
-        <Text>App logo</Text>
+      <View style={styles.logoContainer}>
+        <Image
+          source={require("../../assets/images/app-logo.png")}
+          style={{ width: 100, height: 100, alignSelf: "center" }}
+        />
       </View>
-      <View>
-        <Text>Login Form</Text>
+      <View style={styles.loginContainer}>
+        <TextInput placeholder="Email" />
+        <TextInput placeholder="Password" />
+        <TouchableOpacity>
+          <Text>Login</Text>
+        </TouchableOpacity>
       </View>
-      <View>
-        <Text>Social Logins</Text>
+      <View style={styles.socialLoginContainer}>
+        <Image
+          source={require("../../assets/images/app-logo.png")}
+          style={{ width: 35, height: 35, alignSelf: "center" }}
+        />
+        <Image
+          source={require("../../assets/images/app-logo.png")}
+          style={{ width: 35, height: 35, alignSelf: "center" }}
+        />
       </View>
-      <View>
-        <Text>Account Creation</Text>
+      <View style={styles.accountCreationContainer}>
+        <TouchableOpacity>
+          <Text>Create Account</Text>
+        </TouchableOpacity>
       </View>
     </View>
   );
--- styles/login.ts
+++ styles/login.ts
@@ -2,7 +2,11 @@ import { StyleSheet } from "react-native";
 
 export const styles = StyleSheet.create({
   container: {
-    flex:1,
+    flex: 1,
     backgroundColor: "#FFFFFF",
   },
+  logoContainer: { backgroundColor: "red" },
+  loginContainer: { backgroundColor: "blue" },
+  socialLoginContainer: { backgroundColor: "green", flexDirection: "row", justifyContent: "center" },
+  accountCreationContainer: { backgroundColor: "yellow" },
 });
